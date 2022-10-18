692962c9fc2ff092db25a7d0931439eb2b60245b
oleg <storm8719@gmail.com>
2022-10-18 11:36:59 +0300

    implement some methods

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,8 +1,18 @@
+import api from "../models/tinkoff-api";
+
 const resolver = {
     test() {
         return "123"
     },
+    async getSandboxAccounts(){
+        return await api.getSandboxAccounts();
+    },
+    async getRealAccounts(){
+        return await api.getRealAccounts();
+    }
 
 }
+
+// const resolverTinkoff = api;
 
 export default resolver
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,9 +1,23 @@
 import {buildSchema} from 'graphql'
 
 const schema = buildSchema(`
-    type Query {
-    test: String
+  type Accounts{
+    accounts: [Account!]!
   }
+  type Account{
+    accessLevel: Int,
+    id: String,
+    name: String,
+    openedDate: String,
+    status: Int,
+    type: Int,
+  }
+  type Query {
+    test: String,
+    getSandboxAccounts: Accounts!,
+    getRealAccounts: Accounts!,
+  }
+  
 `)
 
 export default schema;
